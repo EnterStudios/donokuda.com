327b72fe6294df3d977d4b1a652e29935898d96b
Don Okuda <don.okuda@gmail.com>
2014-11-03 22:25:44 -0800

    Upgrade ember-cli

--- app/app.js
+++ app/app.js
@@ -1,16 +1,18 @@
 import Ember from 'ember';
 import Resolver from 'ember/resolver';
 import loadInitializers from 'ember/load-initializers';
+import config from './config/environment';
 import DS from 'ember-data';
 
 Ember.MODEL_FACTORY_INJECTIONS = true;
 
 var App = Ember.Application.extend({
-  modulePrefix: 'portfolio', // TODO: loaded via config
+  modulePrefix: config.modulePrefix,
+  podModulePrefix: config.podModulePrefix,
   Resolver: Resolver
 });
 
-loadInitializers(App, 'portfolio');
+loadInitializers(App, config.modulePrefix);
 
 export default DS.FixtureAdapter.extend();
 export default App;
